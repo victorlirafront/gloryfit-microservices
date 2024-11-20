573ef99fe64b42684cfe0bda59838f09cfdf508c
VictorLira-DEV <victorliradeveloper@gmail.com>
2024-11-20 15:12:51 -0300

    remove useless code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>HOME VIEW</h1>
   </main>
 </template>
