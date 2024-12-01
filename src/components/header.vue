e88b812d380fb5e5ae4ef231a7f4a3da6c625b4d
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-01 13:34:08 -0300

    chore: SCSS config

--- src/components/header.vue
+++ src/components/header.vue
@@ -5,11 +5,14 @@
         <p>E-commerce</p>
       </div>
 
-      <div class="cart-wrapper">
-        <div class="cart">
+      <div class="cart-wrapper" @click="toggleCheckout">
+        <div class="cart" ref="cartText">
           <img class="cart-img" src="../assets/logo.svg" alt="cart icon" />
         </div>
       </div>
+
+      <!-- No Vue.js, a diretiva v-if é usada para renderizar condicionalmente um elemento no DOM. Isso significa que o elemento só será incluído no DOM se a expressão associada ao v-if for avaliada como true. -->
+      <div class="checkout" v-if="isCheckoutVisible"></div>
     </div>
   </header>
 </template>
@@ -17,57 +20,22 @@
 <script lang="ts">
 export default {
   name: 'AppHeader',
-}
+  // A função data() em um componente Vue é usada para definir o estado reativo do componente. O que você retorna dentro dessa função estará disponível como propriedades reativas dentro do componente, e qualquer alteração nessas propriedades causará uma atualização automática da interface do usuário.
+  data() {
+    return {
+      isCheckoutVisible: false,
+    };
+  },
+
+  //Funções que são interações do usuário (como eventos de clique, mudança de input, etc.) geralmente vão dentro de methods.
+  methods: {
+    toggleCheckout() {
+      this.isCheckoutVisible = !this.isCheckoutVisible;
+    },
+  },
+};
 </script>
 
-<style scoped>
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem 2rem;
-  background-color: #121214;
-  color: white;
-  width: 95%;
-  max-width: 100vw;
-  margin: 0 auto;
-}
-
-.container {
-  font-family: sans-serif;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-}
-
-.container .logo-icon {
-  font-size: 20px;
-  font-weight: bold;
-}
-
-.cart-wrapper {
-}
-
-.cart-wrapper .cart {
-  background: #202024;
-  padding: 12px;
-  border-radius: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 50px;
-  height: 50px;
-  cursor: pointer;
-  transition: filter 0.3s ease-in-out;
-}
-
-.cart-wrapper .cart:hover {
-  filter: brightness(1.5);
-}
-
-.cart-wrapper .cart .cart-img {
-  width: 24px;
-  height: auto;
-}
+<style scoped lang="scss">
+@use './header.scss' as *;
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -14,7 +13,14 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Não precisamos de additionalData neste caso, então podemos remover
+      }
+    }
+  }
 })
