db7af58591e9f62c29e6cf4002600f8435e906ab
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-26 20:20:15 -0300

    feat: add main tag

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-
-import AppHeader from "./components/header/header.vue"
-import AppFooter from "./components/footer/footer.vue"
+import AppHeader from './components/header/header.vue'
+import AppFooter from './components/footer/footer.vue'
 </script>
 
 <template>
   <AppHeader />
+  <main></main>
   <AppFooter />
 </template>
 
