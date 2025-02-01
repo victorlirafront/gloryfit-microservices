4ac088091036ee5a933207afafa8f99e0b008f4b
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-01 17:20:18 -0300

    feat: add mock data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,14 +10,8 @@ import { IMAGES } from '@/constants/images'
     <div class="card-wrapper">
       <div class="card">
         <img class="header__cart-img" :src="IMAGES.TSHIRT" alt="cart icon" />
-        <button class="add-to-cart">Adicionar</button>
-      </div>
-      <div class="card">
-        <img class="header__cart-img" :src="IMAGES.TSHIRT" alt="cart icon" />
-        <button class="add-to-cart">Adicionar</button>
-      </div>
-      <div class="card">
-        <img class="header__cart-img" :src="IMAGES.TSHIRT" alt="cart icon" />
+        <p class="title">Camiseta Ignite Lab</p>
+        <p class="price">R$ 79,99</p>
         <button class="add-to-cart">Adicionar</button>
       </div>
     </div>
