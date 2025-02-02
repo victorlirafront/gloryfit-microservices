df3105f2a8cd456a626f16e30707b4befbf5f75d
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-02 17:32:35 -0300

    style: add cart border radius

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@ import { useCartStore } from '@/stores/cart'
 import products from './data/mock_products.json';
 
 const cart = useCartStore()
-const { cartItems, addToCart, removeFromCart, getTotal } = cart
+const { addToCart } = cart
 </script>
 
 <template>
@@ -13,11 +13,12 @@ const { cartItems, addToCart, removeFromCart, getTotal } = cart
   <main class="main">
     <div class="card-wrapper">
       <div v-for="(product, index) in products" :key="index" class="card">
-        <img class="header__cart-img" :src="product.image" alt="cart icon" />
-        <p class="title">{{ product.name }}</p>
-        <p class="price">R$ {{ product.price.toFixed(2) }}</p>
-
-        <button class="add-to-cart" @click="addToCart(product.id)">Adicionar ao Carrinho</button>
+        <div class="info">
+          <img class="header__cart-img" :src="product.image" alt="cart icon" />
+          <p class="title">{{ product.name }}</p>
+            <p class="price">R$ {{ product.price.toFixed(2) }}</p>
+          </div>
+        <button class="add-to-cart" @click="addToCart(product.id)">Adicionar</button>
       </div>
     </div>
   </main>
