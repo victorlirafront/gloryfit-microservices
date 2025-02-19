fbc690e573e075048241aee68fbf94ef8f68cd62
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-19 19:13:58 -0300

    feat: add sign up page

--- frontend/src/views/Login/Login.vue
+++ frontend/src/views/Login/Login.vue
@@ -1,11 +1,24 @@
-<template >
-  <main class="login-wrapper">
-    <form id="form-login" action="">
-      <h1>Login</h1>
+<template>
+  <main class="login">
+    <form class="login__form">
+      <h1 class="login__title-1">Bem vindo ancora store!</h1>
+      <h2 class="login__title-2">Antes de continuar, faça seu login.</h2>
+
+      <div class="login__field">
+        <label class="login__label" for="email">Email</label>
+        <input class="login__input" type="email" id="email" placeholder="Digite seu email" />
+      </div>
+
+      <div class="login__field">
+        <label class="login__label" for="password">Senha</label>
+        <input class="login__input" type="password" id="password" placeholder="Digite sua senha" />
+      </div>
+
+      <button class="login__submit" type="submit">Entrar</button>
+      <button class="login__signup" type="submit">Faça seu cadastro</button>
     </form>
   </main>
 </template>
-
 
 <style scoped lang="scss">
 @use './login.scss' as *;
