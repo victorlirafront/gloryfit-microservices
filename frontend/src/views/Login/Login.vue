ded04cfae0a06a48949172a0ca677c327c79bd9b
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-19 00:07:40 -0300

    style: add form login initial style

--- frontend/src/views/Login/Login.vue
+++ frontend/src/views/Login/Login.vue
@@ -1,5 +1,12 @@
-<template>
-  <main>
-    <h1>Login</h1>
+<template >
+  <main class="login-wrapper">
+    <form id="form-login" action="">
+      <h1>Login</h1>
+    </form>
   </main>
 </template>
+
+
+<style scoped lang="scss">
+@use './login.scss' as *;
+</style>
