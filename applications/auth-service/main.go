509b70f5a172f510708db8af17a966fd572cdf07
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-22 13:39:52 -0300

    chore: add close db

--- applications/auth-service/main.go
+++ applications/auth-service/main.go
@@ -1,5 +1,23 @@
-package authService
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/victorlirafront/ancora/applications/auth-service/database"
+)
 
 func main() {
+	db, err := database.CreateDatabaseConnection()
 
+	if err != nil {
+		//log.Fatalf exibe uma mensagem de erro no log e finaliza o programa com os.Exit(1). No seu caso,
+		// ele informa que houve um erro ao conectar ao banco de dados e encerra a execução.
+		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
+	}
+
+	// "defer" garante que um código será executado no final, mesmo que tenha erros no me
+	defer db.Close() // Fechará o banco de dados no final da função
+
+	fmt.Println("Servidor rodando em localhost:8080")
 }
