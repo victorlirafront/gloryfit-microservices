9a1262c428aee9c116493dca1d8b3cb0cd046a17
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-24 13:05:42 -0300

    chage component name

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home/Home.vue'
 import Login from '@/views/Login/Login.vue'
-import SignUp from '@/views/SignUp/Signup.vue'
+import SignUp from '@/views/SignUp/SignUp.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
