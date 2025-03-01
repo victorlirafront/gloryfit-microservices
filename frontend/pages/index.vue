8431a24e4df9cd6926d4a420418a0197d6c84b8e
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-03-01 18:25:08 -0300

    fix path

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script setup>
-import HomeView from '../src/views/home/home.vue'
-import AppHeader from '../src/components/header/header.vue'
-import AppFooter from '../src/components/footer/footer.vue'
+import HomeView from '~/src/views/home/home.vue';
+import AppHeader from '~/src/components/header/header.vue';
+import AppFooter from '~/src/components/footer/footer.vue';
 
 definePageMeta({
   title: 'Página Inicial',
@@ -17,5 +17,5 @@ definePageMeta({
       content: 'Esta é a página inicial do nosso site',
     },
   ],
-})
+});
 </script>
